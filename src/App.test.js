ebda6a6aed3855082515e852f0c77bb52180940e
Magne Alvheim <magne@alvheim.com>
2020-03-10 10:24:35 +0100

    mere tester

--- src/App.test.js
+++ src/App.test.js
@@ -3,9 +3,17 @@ import { render } from '@testing-library/react';
 import App from './App';
 import util from 'util'
 
-test('renders learn react link', () => {
+test('renders app', () => {
   const { getByText } = render(<App />);
   console.log(util.inspect(getByText))
   const linkElement = getByText(/Faggruppe workshop/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+test('renders hello', () => {
+  const HelloWorld = () => <h1>Hello World</h1>
+  const { debug } = render(<HelloWorld />)
+  debug()
+});
+
--- src/calculator/calculatorComponent.test.js
+++ src/calculator/calculatorComponent.test.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, getAllByTestId, getByTestId } from '@testing-library/react';
 import CalculatorComponent from './calculatorComponent';
 import util from 'util'
 
 test('Render CalculatorComponent', () => {
     const { calcComp } = render(<CalculatorComponent />);
-    console.log('util.inspect(calcComp)')
-    console.log(util.inspect(calcComp))
-    //const linkElement = getByText(/Faggruppe workshop/i);
-    //expect(linkElement).toBeInTheDocument();
+    //console.log('util.inspect(calcComp)')
+    //console.log(util.inspect(calcComp))
+    const linkElement = getByText(/Faggruppe workshop/i);
+    expect(linkElement).toBeInTheDocument();
+});
+
+
+test('Dumps CalculatorComponent', () => {
+    //const HelloWorld = () => <h1>Hello World</h1>
+    const { debug } = render(<CalculatorComponent />)
+    //debug()
+});
+
+test('Render CalculatorComponent Find A', () => {
+    const { getByTestId } = render(<CalculatorComponent />);
+    const htmlElement = getByTestId("a");
+    expect(htmlElement).toBeInTheDocument();
+});
+
+test('Render CalculatorComponent Find B', () => {
+    const { getByTestId } = render(<CalculatorComponent />);
+    const htmlElement = getByTestId("b");
+    htmlElement.se
+    expect(htmlElement).toBeInTheDocument();
 });
